d6388bb61df3215393840f59ccbcb52684ddb244
tuchin1228 <1104108110@nkust.edu.tw>
2020-11-12 23:33:52 +0800

    alert correct

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App(props) {
 
   return (
     <MuiThemeProvider theme={theme}>
-      {status ? (
+      {status.type ? (
         <Snackbar
           open={openAlert}
           autoHideDuration={2000}

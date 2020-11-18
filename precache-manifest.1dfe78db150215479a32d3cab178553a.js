78673c4da0cf3e5ccc1d01fd9e429abb05acfee2
tuchin1228 <1104108110@nkust.edu.tw>
2020-11-18 21:25:28 +0800

    dist

---
+++ precache-manifest.1dfe78db150215479a32d3cab178553a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd1975e4bfce6f06a2e844aa3ae418d",
+    "url": "/react-kaigang/index.html"
+  },
+  {
+    "revision": "4dcb6040d45ec53b1e17",
+    "url": "/react-kaigang/static/css/main.51f94b0b.chunk.css"
+  },
+  {
+    "revision": "6699a555a1b06c880a92",
+    "url": "/react-kaigang/static/js/2.bb0fc827.chunk.js"
+  },
+  {
+    "revision": "2b1b7514e26d7fa0a3827cd22324cee7",
+    "url": "/react-kaigang/static/js/2.bb0fc827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dcb6040d45ec53b1e17",
+    "url": "/react-kaigang/static/js/main.bb9b8fd2.chunk.js"
+  },
+  {
+    "revision": "d43db4636435338091ba",
+    "url": "/react-kaigang/static/js/runtime-main.effaa9e7.js"
+  },
+  {
+    "revision": "2eb70de6d8cc81a163291a860e9a3621",
+    "url": "/react-kaigang/static/media/404.2eb70de6.png"
+  }
+]);
